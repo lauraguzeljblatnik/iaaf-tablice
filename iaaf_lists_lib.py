99c47aa7844639a023d7c1df0e02471c2ce24f66
Laura Guzelj Blatnik <laura.guzelj-blatnik@student.fmf.uni-lj.si>
2017-12-27 00:01:30 +0100

    dopolnila funkcije

--- iaaf_lists_lib.py
+++ iaaf_lists_lib.py
@@ -18,25 +18,53 @@ where = ['outdoor', 'indoor']
 gender = ['women', 'men']
 agegroup = ['senior', 'u18', 'u20']
 
+re_block_result = re.compile(
+    r'<tr data-id="\d+" >(?P<block>.*?)</tr>',
+    flags=re.DOTALL
+)
 
 re_competitor = re.compile(
     r'<a href="/athletes/athlete=(?P<id>\d+)">'
-    r'(?P<name>.*?).*?(?P<surname>.*?)</span>',
+    r'(?P<name>.*?)<span class=.name-uppercase.>(?P<surname>.*?)</span>',
     re.DOTALL
     )
 
-rx_info = re.compile(
+re_result_info = re.compile(
     r'<td data-th="Rank">(?P<rank>.*?)</td>'
+    r'.*?'
     r'<td data-th="Mark">(?P<time>.*?)</td>'
     r'.*?'
-    r' <td data-th="DOB">(?P<DOB>.*?)</td>'
-    r'<td data-th="Nat"><.*?/>(?P<nationality>.*?)</td>'
+    r'<td data-th="Competitor">.*?<a href="/athletes/athlete=(?P<id>\d+)">'
+    r'(?P<name>.*?) <span class=.name-uppercase.>(?P<surname>.*?)</span></a>'
+    r'.*?'
+    r'<td data-th="DOB">(?P<DOB>.*?)</td>'
+    r'.*?'
+    r'<td data-th="Nat">.*?<img.*?/>(?P<nationality>.*?)</td>'
+    r'.*?'
     r'<td data-th="Pos">(?P<position>.*?)</td>'
     r'.*?'
     r'<td data-th="Venue">(?P<venue>.*?)</td>'
+    r'.*?'
     r'<td data-th="Date">(?P<date>.*?)</td>',
     re.DOTALL
     )
+
+
+def result_info(single_result):
+    match = re_result_info.search(single_result)
+    if match:
+        result = match.groupdict()
+##        result['rank'] = int(result['rank']) if result['rank'] else None
+##        result['time'] = result['time'].strip
+##        result['DOB'] = result['DOB']
+        #print(result['rank'], result['time'], result['DOB'])
+        return result
+        print('match')
+    else:
+        print('cannot read this result, im useless :(')
+        print(single_result)
+
+
 
 #choose category, discipline, where, gender and agegroup from lists upwards
 def download_all_time_lists(
@@ -51,7 +79,6 @@ def download_all_time_lists(
                 '{}/{}/{}/{}/{}/{}'
             ).format(category, discipline, where, gender, agegroup, year)
             r = requests.get(url)
-            print(url)
         except requests.exceptions.RequestException as e:
             print(e)
             return None
@@ -63,24 +90,24 @@ def download_all_time_lists(
         with open(path, 'w', encoding='utf-8') as file_out:
             file_out.write(r.text)
 
-def split_lists(page_content):
-    rx = re.compile(
-        r'<tr data-id="\d+">(.*?)</td>',
-        re.DOTALL
-        )
-    matches = re.findall(rx,page_content)
-    return matches
 
-#split to rank
-#read file
-
-
+def read_lists(directory):
+    results = []
+    for file_name in os.listdir(directory):
+        path = os.path.join(directory, file_name)
+        with open(path, 'r') as file:
+            file_content = file.read()
+            for single_result in re_block_result.finditer(file_content):
+                results.append(result_info(single_result.group(0)))
+    return results
 
 
+#UKAZI
 
+##download_all_time_lists(
+##    'middlelong', '5000-metres', 'outdoor', 'women', 'senior', lists_directory
+##    )
 
-
-
-
+lists = read_lists(lists_directory)
 
 
